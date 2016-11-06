890d91ec60a8daedaa7bec7c2260f5811c6f3779
jack2gs <sailor.g@163.com>
2016-11-06 09:03:23 +0800

    Initial Commit

---
+++ IPv6Synthesization/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  IPv6Synthesization
+//
+//  Created by Gao Song on 06/11/2016.
+//  Copyright © 2016 Gao Song. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
